d350e2e0b7b6608fb7cf7dc2b3ec379ecf86665a
codesmitty <varcodesmitty@gmail.com>
2020-06-08 10:21:26 -0400

    added custom button and custom signIn page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
-
+import SignInSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-sign-up.component'
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -12,14 +12,17 @@ const HatsPage = () => (
 )
 
 class App extends Component {
+
+  
   render() {
     return <div className='App'>
       <Header /> 
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signup" component={SignInSignUpPage} />
     </Switch>
-    </div>;
+    </div>
   }
 }
 
