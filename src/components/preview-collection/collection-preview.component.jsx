918602901eef9969ab5d3e482303731a57c84175
codesmitty <varcodesmitty@gmail.com>
2020-06-12 12:03:53 -0400

    added utils file

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
@@ -10,13 +11,15 @@ const CollectionPreview = ({title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {items.filter((item, idx) => idx < 4)
-                .map(({id, ...otherItemProps })=> (
-                    <CollectionItem key={id} {...otherItemProps} />
+                .map((item)=> (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
     </div>
 );
+
+
 
 
 export default CollectionPreview;
